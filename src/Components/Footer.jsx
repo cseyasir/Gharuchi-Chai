550fef29f810a30cc4faf329cf32d87516b05a77
Yasir Arfat <yasirarfatlglglg@gmail.com>
2026-05-01 09:50:36 +0530

    fix useEffect dependency issue

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-white text-center p-3 mt-5">
       <p className="mb-1">© 2026 GarxechChai</p>
-      <small>Powered by GharuchChai</small>
+      <small>Powered by GarxechChai</small>
     </footer>
   );
 }
